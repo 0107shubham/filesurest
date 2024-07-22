75a88255ac28dd4ce821abd101942603a915fd6f
shubham tyagi <shubhamtyagi.lncte@gmail.com>
2024-07-22 17:05:16 +0530

    update home

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Contact from "./components/Contact";
 import Development from "./components/Development";
 import FAQ from "./components/FAQ";
 import Footer from "./components/Footer";
+import Home from "./components/Home";
 import Media from "./components/Media";
 import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
@@ -13,6 +14,7 @@ function App() {
   return (
     <div className="flex flex-col items-center bg-[#f2f4f7]">
       <Navbar />
+      <Home />
       <Services />
       <Media />
       <Projects />
