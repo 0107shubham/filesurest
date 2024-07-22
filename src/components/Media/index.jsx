cb1a81252ed211d7106e11c2e632d01b41698b31
shubham tyagi <shubhamtyagi.lncte@gmail.com>
2024-07-22 21:45:34 +0530

    update media

--- src/components/Media/index.jsx
+++ src/components/Media/index.jsx
@@ -1,5 +1,19 @@
 import React from "react";
 import socilaMedia from "../../assets/socialmedia.png";
+import md1 from "../../assets/md1.png";
+import md2 from "../../assets/md2.png";
+import md3 from "../../assets/md3.png";
+import md4 from "../../assets/md4.png";
+import md5 from "../../assets/md5.png";
+import md6 from "../../assets/md6.png";
+import md7 from "../../assets/md7.png";
+import md8 from "../../assets/md8.png";
+import md9 from "../../assets/md9.png";
+import md10 from "../../assets/md10.png";
+import md11 from "../../assets/md11.png";
+import md12 from "../../assets/md12.png";
+import md13 from "../../assets/md13.png";
+import md14 from "../../assets/md14.png";
 
 const Media = () => {
   return (
@@ -26,171 +40,105 @@ const Media = () => {
           </div>
           <div className="flex items-center">
             <div className="bg-white p-3 rounded-full">
-              <img
-                src={socilaMedia}
-                alt="socialMedia"
-                className="h-[22px] w-[20px] "
-              />
+              <img src={md1} alt="md1" className="h-[22px] w-[20px] " />
             </div>
             <p className="text-[17px] font-normal leading-6 mx-2">
-              Social media
+              Fitness and sport
             </p>
           </div>
           <div className="flex items-center">
             <div className="bg-white p-3 rounded-full">
-              <img
-                src={socilaMedia}
-                alt="socialMedia"
-                className="h-[22px] w-[20px] "
-              />
+              <img src={md2} alt="md2" className="h-[22px] w-[20px] " />
+            </div>
+            <p className="text-[17px] font-normal leading-6 mx-2">Bank</p>
+          </div>
+          <div className="flex items-center">
+            <div className="bg-white p-3 rounded-full">
+              <img src={md3} alt="md3" className="h-[22px] w-[20px] " />
             </div>
             <p className="text-[17px] font-normal leading-6 mx-2">
-              Social media
+              Construction
             </p>
           </div>
           <div className="flex items-center">
             <div className="bg-white p-3 rounded-full">
-              <img
-                src={socilaMedia}
-                alt="socialMedia"
-                className="h-[22px] w-[20px] "
-              />
+              <img src={md4} alt="md4" className="h-[22px] w-[20px] " />
             </div>
             <p className="text-[17px] font-normal leading-6 mx-2">
-              Social media
+              Game projects
             </p>
           </div>
           <div className="flex items-center">
             <div className="bg-white p-3 rounded-full">
-              <img
-                src={socilaMedia}
-                alt="socialMedia"
-                className="h-[22px] w-[20px] "
-              />
+              <img src={md5} alt="md5" className="h-[22px] w-[20px] " />
+            </div>
+            <p className="text-[17px] font-normal leading-6 mx-2">Education </p>
+          </div>
+          <div className="flex items-center">
+            <div className="bg-white p-3 rounded-full">
+              <img src={md6} alt="md6" className="h-[22px] w-[20px] " />
             </div>
             <p className="text-[17px] font-normal leading-6 mx-2">
-              Social media
+              Auto, transport
             </p>
           </div>
           <div className="flex items-center">
             <div className="bg-white p-3 rounded-full">
-              <img
-                src={socilaMedia}
-                alt="socialMedia"
-                className="h-[22px] w-[20px] "
-              />
+              <img src={md7} alt="md7" className="h-[22px] w-[20px] " />
             </div>
             <p className="text-[17px] font-normal leading-6 mx-2">
-              Social media
+              Medicine, health
             </p>
           </div>
           <div className="flex items-center">
             <div className="bg-white p-3 rounded-full">
-              <img
-                src={socilaMedia}
-                alt="socialMedia"
-                className="h-[22px] w-[20px] "
-              />
+              <img src={md8} alt="md8" className="h-[22px] w-[20px] " />
             </div>
             <p className="text-[17px] font-normal leading-6 mx-2">
-              Social media
+              Restaurants, food delivery
             </p>
           </div>
           <div className="flex items-center">
             <div className="bg-white p-3 rounded-full">
-              <img
-                src={socilaMedia}
-                alt="socialMedia"
-                className="h-[22px] w-[20px] "
-              />
+              <img src={md9} alt="md9" className="h-[22px] w-[20px] " />
             </div>
             <p className="text-[17px] font-normal leading-6 mx-2">
-              Social media
+              Online cources
             </p>
           </div>
           <div className="flex items-center">
             <div className="bg-white p-3 rounded-full">
-              <img
-                src={socilaMedia}
-                alt="socialMedia"
-                className="h-[22px] w-[20px] "
-              />
+              <img src={md10} alt="md10" className="h-[22px] w-[20px] " />
             </div>
             <p className="text-[17px] font-normal leading-6 mx-2">
-              Social media
+              Marketplaces{" "}
             </p>
           </div>
           <div className="flex items-center">
             <div className="bg-white p-3 rounded-full">
-              <img
-                src={socilaMedia}
-                alt="socialMedia"
-                className="h-[22px] w-[20px] "
-              />
+              <img src={md11} alt="md11" className="h-[22px] w-[20px] " />
             </div>
             <p className="text-[17px] font-normal leading-6 mx-2">
-              Social media
+              AR technology
             </p>
           </div>
           <div className="flex items-center">
             <div className="bg-white p-3 rounded-full">
-              <img
-                src={socilaMedia}
-                alt="socialMedia"
-                className="h-[22px] w-[20px] "
-              />
+              <img src={md12} alt="md12" className="h-[22px] w-[20px] " />
             </div>
-            <p className="text-[17px] font-normal leading-6 mx-2">
-              Social media
-            </p>
+            <p className="text-[17px] font-normal leading-6 mx-2">TV series</p>
           </div>
           <div className="flex items-center">
             <div className="bg-white p-3 rounded-full">
-              <img
-                src={socilaMedia}
-                alt="socialMedia"
-                className="h-[22px] w-[20px] "
-              />
+              <img src={md13} alt="md13" className="h-[22px] w-[20px] " />
             </div>
-            <p className="text-[17px] font-normal leading-6 mx-2">
-              Social media
-            </p>
+            <p className="text-[17px] font-normal leading-6 mx-2">Startups</p>
           </div>
           <div className="flex items-center">
             <div className="bg-white p-3 rounded-full">
-              <img
-                src={socilaMedia}
-                alt="socialMedia"
-                className="h-[22px] w-[20px] "
-              />
+              <img src={md14} alt="md14" className="h-[22px] w-[20px] " />
             </div>
-            <p className="text-[17px] font-normal leading-6 mx-2">
-              Social media
-            </p>
-          </div>
-          <div className="flex items-center">
-            <div className="bg-white p-3 rounded-full">
-              <img
-                src={socilaMedia}
-                alt="socialMedia"
-                className="h-[22px] w-[20px] "
-              />
-            </div>
-            <p className="text-[17px] font-normal leading-6 mx-2">
-              Social media
-            </p>
-          </div>
-          <div className="flex items-center">
-            <div className="bg-white p-3 rounded-full">
-              <img
-                src={socilaMedia}
-                alt="socialMedia"
-                className="h-[22px] w-[20px] "
-              />
-            </div>
-            <p className="text-[17px] font-normal leading-6 mx-2">
-              Social media
-            </p>
+            <p className="text-[17px] font-normal leading-6 mx-2">Religion </p>
           </div>
         </div>
       </div>
